c766f4c96c80e7a69a2826097bf9e020660638ed
M1cha3l <michaeltimothy@live.ca>
2024-12-20 02:17:37 -0400

    UI improvements
    The UI for the form has been updated.

--- callsubmissionpython/app.py
+++ callsubmissionpython/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Azure Blob Storage setup
-AZURE_STORAGE_CONNECTION_STRING = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNoWJ1XDu3MSmf1234567890+FYZ1WOwYcpruEclxYYvIQ==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;"
+AZURE_STORAGE_CONNECTION_STRING = "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;"
 CONTAINER_NAME = "callfiles"
 
 # Initialize BlobServiceClient
